217e232a52d7487482f5efca7be10108f498fd4d
Christopher Lum <lum@uw.edu>
2024-12-06 19:01:13 -0800

    Adding LumMisc project

---
+++ src/LumMisc.cpp
@@ -0,0 +1,5 @@
+#include "LumMisc.h"
+
+bool IsBit1(unsigned char x, unsigned char position) {
+	return 0;
+}
---
+++ src/LumMisc.h
@@ -0,0 +1,6 @@
+#ifndef GUARD_LUMMISC_H
+#define GUARD_LUMMISC_H
+
+bool IsBit1(unsigned char x, unsigned char position);
+	
+#endif
